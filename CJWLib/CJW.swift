7e297a0575b55ba3a8c16fe8e2f835b051217927
cjw <fk911c@gmail.com>
2015-09-08 13:50:31 +0800

    update

---
+++ CJWLib/CJW.swift
@@ -0,0 +1,15 @@
+//
+//  CJW.swift
+//  CJWLib
+//
+//  Created by Frank on 9/8/15.
+//  Copyright (c) 2015 frank. All rights reserved.
+//
+
+import UIKit
+
+class CJW: NSObject {
+    func testing(){
+        println("testing")
+    }
+}
